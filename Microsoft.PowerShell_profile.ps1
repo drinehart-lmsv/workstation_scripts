5131b58c65156eccba4c60449decdfd5ac5100eb
Duane Rinehart <duane.rinehart@lmsvsd.net>
2026-03-06 14:31:59 -0800

    added prompt mod to display hostname

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,17 @@
-# 5-MAR-2026
+# 6-MAR-2026
 # AUTHOR: Duane Rinehart (duane.rinehart@lmsvsd.net)
 # Purpose: Windows Powershell user profile mod to emulate linux 'which' command
 
 # HOWTO:
-# 1) Open Powershell and run: code $PROFILE
+# 1) Open Powershell and run: notepad $PROFILE
 # 2) Paste the following code into the profile file and save it.
 
 function which ($Command) {
     $path = Get-Command $Command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path
     if ($path) { return $path } 
     else { Write-Warning "Command '$Command' not found in PATH." }
+}
+
+function prompt {
+    "[$env:COMPUTERNAME] $(Get-Location) > "
 }
